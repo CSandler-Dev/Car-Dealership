8f293164e8291d4b4b721834b3294742155d3101
Caleb Sandler <sandlercaleb@gmail.com>
2024-11-15 16:38:27 -0500

    Hi
    please work

---
+++ src/adminLogin.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class adminLogin {
+
+    Scanner scnr = new Scanner(System.in);
+
+    public int showMenu() {
+        System.out.println("Welcome Admin!");
+        System.out.println("1. Login with your code");
+        System.out.println("2. Become an admin");
+        System.out.print("Menu Choice: ");
+        int code = scnr.nextInt();
+
+        return code;
+    }
+
+    public void createAdmin(String name, int code){
+        ArrayList adminLog = new ArrayList();
+    }
+
+    public void codeLogin(int code){
+        int userCode;
+        userCode = code;
+
+    }
+}
